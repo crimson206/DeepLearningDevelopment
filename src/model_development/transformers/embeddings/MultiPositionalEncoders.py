99549f0188a9da0c216762f61cb97be4af8ea294
Crimson206 <sisung.kim1@gmail.com>
2023-11-10 15:40:05 +0100

    added MultiPositionalEncoders
    with BiDirectional Example

---
+++ src/model_development/transformers/embeddings/MultiPositionalEncoders.py
@@ -0,0 +1,33 @@
+from PositionalEncoders import ArbitraryPositionalEncoder, PositionalEncoder
+
+import torch
+import torch.nn as nn
+
+class MultiPositionalEncoder(nn.Module):
+    def __init__(self, max_lengths, embedding_dims):
+        super(MultiPositionalEncoder, self).__init__()
+        self.encodings = nn.ModuleList([
+            PositionalEncoder(d_emb, max_len) for max_len, d_emb in zip(max_lengths, embedding_dims)
+        ])
+
+    def forward(self, *positional_ids):
+        encoded_features = [encoding(pos_id) for encoding, pos_id in zip(self.encodings, positional_ids)]
+        combined_encoding = torch.cat(encoded_features, dim=-1)
+        return combined_encoding
+    
+class MultiArbitraryPositionalEncoder(nn.Module):
+    def __init__(self, max_lengths, embedding_dims):
+        super(MultiArbitraryPositionalEncoder, self).__init__()
+        self.encodings = nn.ModuleList([
+            ArbitraryPositionalEncoder(max_pos_len, d_emb) for max_pos_len, d_emb in zip(max_lengths, embedding_dims)
+        ])
+
+    def forward(self, *positional_ids, attention_mask):
+        encoded_features = [encoding(pos_id.long()) for encoding, pos_id in zip(self.encodings, positional_ids)]
+        combined_encoding = torch.cat(encoded_features, dim=-1)
+
+        if attention_mask is not None:
+            expanded_mask = attention_mask.unsqueeze(-1).expand_as(combined_encoding)
+            combined_encoding = combined_encoding * expanded_mask
+
+        return combined_encoding
---
+++ src/model_development/transformers/embeddings/PositionalEncoders.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+import math
+
+class PositionalEncoder(nn.Module):
+    def __init__(self, d_model, max_len):
+        super(PositionalEncoder, self).__init__()
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:, :x.size(1)]
+        return x
+
+class ArbitraryPositionalEncoder(nn.Module):
+    def __init__(self, max_pos_len, d_emb):
+        super(ArbitraryPositionalEncoder, self).__init__()
+        self.d_emb = d_emb
+        self.max_pos_len = max_pos_len
+        self.encodings = get_sinusoidal_encoding(self.max_pos_len + 1, self.d_emb)
+
+    def forward(self, input_seqs):
+        """
+        Apply sinusoidal positional encoding to the input batch of sequences using custom positions.
+
+        Args:
+        input_seqs: A 2D torch.Tensor of shape (n_batch, n_seq) containing sequences of positions.
+
+        Returns:
+        A 3D torch.Tensor of shape (n_batch, n_seq, d_emb) containing the positional encodings for the batch.
+        """
+        batch_encodings = []
+        for sequence in input_seqs:
+            # Retrieve the relevant encodings for the current sequence
+            sequence_encodings = torch.stack([self.encodings[pos] for pos in sequence])
+            batch_encodings.append(sequence_encodings)
+
+        # Get the positional encodings for the batch
+        batch_encodings = torch.stack(batch_encodings)
+
+        return batch_encodings
+
+def get_sinusoidal_encoding(max_seq_len, d_emb):
+    """
+    Generate sinusoidal positional encodings for a given sequence length and dimensionality.
+
+    Args:
+    max_seq_len: The maximum length of the input sequences for which to generate encodings.
+    d_emb: The dimensionality of the embeddings.
+
+    Returns:
+    A 2D torch.Tensor of shape (max_seq_len, d_emb) containing the positional encodings.
+    """
+    position = torch.arange(max_seq_len).unsqueeze(1).float()
+    div_term = torch.exp(torch.arange(0, d_emb, 2).float() * -(math.log(10000.0) / d_emb))
+    encoding = torch.zeros(max_seq_len, d_emb)
+    encoding[:, 0::2] = torch.sin(position * div_term)
+    encoding[:, 1::2] = torch.cos(position * div_term)
+    return encoding
