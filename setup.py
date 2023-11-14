fad4f6a96460dbf3f37d9c5027a7721b1033a343
Crimson206 <sisung.kim1@gmail.com>
2023-11-14 17:33:20 +0100

    modified setup

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     version="0.1.3",
     description="Personal Deep Learning Module",
     author="Sisung Kim",
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     install_requires=[
         "numpy>=1,<2",
         "torch>=2,<3",
