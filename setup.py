8855ff3fa4f644e5d3df525e5c08d025157d62b7
Crimson206 <sisung.kim1@gmail.com>
2023-11-24 12:55:54 +0100

    editted setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="DeepLearningDevelopment",
-    version="0.1.4",
+    version="0.1.5",
     description="Personal Deep Learning Module",
     author="Sisung Kim",
     package_dir={"": "src"},
