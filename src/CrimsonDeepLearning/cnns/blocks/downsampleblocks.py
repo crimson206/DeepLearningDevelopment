79c9e06f8de145724ee373a84fed598bdb16ebe5
Crimson206 <sisung.kim1@gmail.com>
2023-11-30 11:41:35 +0100

    initialized downsampleblocks

---
+++ src/CrimsonDeepLearning/cnns/blocks/downsampleblocks.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+from typing import List
+
+class ConvDownsampleBlocks(nn.Module):
+    """
+    Shape Summary:
+    Input tensor shape:
+    (n_batch, hidden_sizes[0], height, width)
+
+    Output tensor shape:
+    (n_batch, hidden_sizes[-1], height / size_down, width / size_down)
+    where / size_down = (/_up 2) ** (len(hidden_sizes) - 1)
+    """
+    def __init__(self, hidden_sizes: List[int]) -> None:
+        super(ConvDownsampleBlocks, self).__init__()
+        layers: List[nn.Module] = []
+        for i in range(len(hidden_sizes) - 1):
+            layers.append(nn.Conv2d(hidden_sizes[i], hidden_sizes[i+1], kernel_size=3, stride=2, padding=1, bias=False))
+            layers.append(nn.InstanceNorm2d(hidden_sizes[i+1]))
+            layers.append(nn.ReLU(inplace=True))
+
+        self.downsample = nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Shape Summary:
+        x.shape: (n_batch, hidden_sizes[0], height, width).
+
+        output.shape:
+        (n_batch, hidden_sizes[-1], height / size_down, width / size_down)
+        where / size_down = (/_up 2) ** (len(hidden_sizes) - 1)
+        """
+        return self.downsample(x)
+
+class ConvDownsampleBlock(nn.Module):
+    def __init__(self, hidden_sizes: List[int]) -> None:
+        super(ConvDownsampleBlock, self).__init__()
+        layers: List[nn.Module] = []
+
+        layers.append(nn.Conv2d(hidden_sizes[0], hidden_sizes[1], kernel_size=3, stride=2, padding=1, bias=False))
+        layers.append(nn.InstanceNorm2d(hidden_sizes[1]))
+        layers.append(nn.ReLU(inplace=True))
+
+        self.downsample = nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.downsample(x)
+
+class DownsampleBlock(nn.Module):
+    def __init__(self, down_channel, down_module=nn.MaxPool2d(kernel_size=2)) -> None:
+        super(DownsampleBlock, self).__init__()
+        layers: List[nn.Module] = []
+
+        layers.append(down_module)
+        layers.append(nn.InstanceNorm2d(down_channel))
+        layers.append(nn.ReLU(inplace=True))
+
+        self.downsample = nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.downsample(x)
