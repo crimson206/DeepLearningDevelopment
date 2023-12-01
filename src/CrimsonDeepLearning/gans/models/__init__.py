09440229bd693ede5db4048b3c45a37e5af90c0a
Crimson206 <sisung.kim1@gmail.com>
2023-12-01 21:12:58 +0100

    added __init__s

---
+++ src/CrimsonDeepLearning/gans/models/__init__.py
@@ -0,0 +1,4 @@
+import sys
+
+sys.path.append('c:/Users/sisun/VSCode/DeepLearningDevelopment/src/CrimsonDeepLearning')
+sys.path.append('c:/Users/sisun/VSCode/DeepLearningDevelopment/src/CrimsonDeepLearning/gans')
