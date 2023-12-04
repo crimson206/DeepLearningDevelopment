84fba87d17fb96cac04b9ecefd45c4f8f0408992
Crimson206 <sisung.kim1@gmail.com>
2023-12-04 17:50:05 +0100

    added dict_to_device

--- src/CrimsonDeepLearning/utils/data.py
+++ src/CrimsonDeepLearning/utils/data.py
@@ -1,3 +1,4 @@
+import torch
 from torch.utils.data import Dataset
 
 class DictionaryDataset(Dataset):
@@ -10,3 +11,12 @@ class DictionaryDataset(Dataset):
 
     def __getitem__(self, idx):
         return {key: value[idx] for key, value in self.data.items()}
+
+def dict_to_device(data, device, dtype=torch.float16):
+    new_data = {}
+    for key, tensor in data.items():
+        tensor = tensor.to(device)
+        if dtype and tensor.is_floating_point():
+            tensor = tensor.to(dtype)
+        new_data[key] = tensor
+    return new_data
