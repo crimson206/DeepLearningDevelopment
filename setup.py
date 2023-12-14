7c462490aa4ccd9f2ea5ef983b217e56afe5db2d
Crimson206 <sisung.kim1@gmail.com>
2023-12-14 16:55:06 +0100

    updatetd version0_1_9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="DeepLearningDevelopment",
-    version="0.1.8",
+    version="0.1.9",
     description="Personal Deep Learning Module",
     author="Sisung Kim",
     package_dir={"": "src"},
