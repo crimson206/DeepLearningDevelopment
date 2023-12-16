cfb9d8963ed5ab4d76debb7b260c925a0f8cef36
Crimson206 <sisung.kim1@gmail.com>
2023-12-16 14:41:27 +0100

    added listdataset

--- src/CrimsonDeepLearning/utils/data.py
+++ src/CrimsonDeepLearning/utils/data.py
@@ -1,5 +1,8 @@
 import torch
 from torch.utils.data import Dataset
+from torchvision import transforms
+import random
+
 
 class DictionaryDataset(Dataset):
     def __init__(self, data):
@@ -20,3 +23,41 @@ def dict_to_device(data, device, dtype=torch.float16):
             tensor = tensor.to(dtype)
         new_data[key] = tensor
     return new_data
+
+
+class ListDataset(Dataset):
+    def __init__(self, data_list):
+        self.data_list = data_list
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, idx):
+        return self.data_list[idx]
+
+
+
+
+class CustomImageDataset(Dataset):
+    def __init__(self, images, labels, transform=None):
+        self.images = images
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image = self.images[idx]
+        label = self.labels[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+class RandomRotationTransform:
+    def __init__(self, angles):
+        self.angles = angles
+
+    def __call__(self, x):
+        angle = random.choice(self.angles)  # Choose a random angle
+        return transforms.functional.rotate(x, angle)
