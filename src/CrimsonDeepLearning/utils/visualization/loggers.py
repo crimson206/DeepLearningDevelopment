eb936d131b4cb0ef6de1dc1d5e5cef9f24a69b51
Crimson206 <sisung.kim1@gmail.com>
2023-12-17 01:10:11 +0100

    made logger keep the whole batch data

--- src/CrimsonDeepLearning/utils/visualization/loggers.py
+++ src/CrimsonDeepLearning/utils/visualization/loggers.py
@@ -3,28 +3,35 @@ import numpy as np
 class Logger():
     def __init__(self):
         self.epoch_values_dict = {}
+        self.batch_values_temp_dict = {}
         self.batch_values_dict = {}
 
     @property
     def batch_dict(self):
         value_dict = {}
-        for key, losses in self.batch_values_dict.items():
+        for key, losses in self.batch_values_temp_dict.items():
             value_dict[key] = losses[-1] if losses else None
         return value_dict
 
     def batch_step(self, values, labels):
         for loss, label in zip(values, labels):
+            if label in self.batch_values_temp_dict.keys():
+                self.batch_values_temp_dict[label].append(loss)
+            else:
+                self.batch_values_temp_dict[label] = [loss]
             if label in self.batch_values_dict.keys():
                 self.batch_values_dict[label].append(loss)
             else:
                 self.batch_values_dict[label] = [loss]
+
+
     
     def epoch_step(self):
-        for label, values in self.batch_values_dict.items():
+        for label, values in self.batch_values_temp_dict.items():
             avg_value = np.mean(values)
             if label in self.epoch_values_dict.keys():
                 self.epoch_values_dict[label].append(avg_value)
             else:
                 self.epoch_values_dict[label] = [avg_value]
     
-        self.batch_values_dict = {}
+        self.batch_values_temp_dict = {}
