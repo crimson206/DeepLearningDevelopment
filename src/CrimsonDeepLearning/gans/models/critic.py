66ca7529501a65a79089044321065ee31469a03d
Crimson206 <sisung.kim1@gmail.com>
2023-12-25 16:43:33 +0100

    removed sampler

--- src/CrimsonDeepLearning/gans/models/critic.py
+++ src/CrimsonDeepLearning/gans/models/critic.py
@@ -8,33 +8,7 @@ import sys
 
 from CrimsonDeepLearning.gans.layers.equalized_layers import EqualizedConv2d, EqualizedLinear
 
-import torch.nn.functional as F
-class Smooth(nn.Module):
-    def __init__(self):
-        super().__init__()
-        kernel = [[1, 2, 1],
-                  [2, 4, 2],
-                  [1, 2, 1]]
-        kernel = torch.tensor([[kernel]], dtype=torch.float)
-        kernel /= kernel.sum()
-        self.kernel = nn.Parameter(kernel, requires_grad=False)
-        self.pad = nn.ReplicationPad2d(1)
 
-    def forward(self, x: torch.Tensor):
-        b, c, h, w = x.shape
-        x = x.view(-1, 1, h, w)
-        x = self.pad(x)
-        x = F.conv2d(x, self.kernel)
-        return x.view(b, c, h, w)
-
-class DownSample(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.smooth = Smooth()
-
-    def forward(self, x: torch.Tensor):
-        x = self.smooth(x)
-        return F.interpolate(x, (x.shape[2] // 2, x.shape[3] // 2), mode='bilinear', align_corners=False)
 
 
 class CriticBlock(nn.Module):
