714f6217a027900034868608acec608a33f07eb4
Crimson206 <sisung.kim1@gmail.com>
2023-12-25 16:47:26 +0100

    added contrastive losses

--- src/CrimsonDeepLearning/utils/losses.py
+++ src/CrimsonDeepLearning/utils/losses.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 import numpy as np
 
@@ -46,3 +47,28 @@ class FlattenLoss(nn.Module):
         label = label.flatten(0, 1)
 
         return self.criterion(pred, label)
+
+def info_nce_loss(feats, temperature=0.7):
+    # Calculate cosine similarity
+    cos_sim = F.cosine_similarity(feats[:, None, :], feats[None, :, :], dim=-1)
+    # Mask out cosine similarity to itself
+    self_mask = torch.eye(cos_sim.shape[0], dtype=torch.bool, device=cos_sim.device)
+    cos_sim.masked_fill_(self_mask, -9e15)
+    # Find positive example -> batch_size//2 away from the original example
+    pos_mask = self_mask.roll(shifts=cos_sim.shape[0] // 2, dims=0)
+    # InfoNCE loss
+    cos_sim = cos_sim / temperature
+    nll = -cos_sim[pos_mask] + torch.logsumexp(cos_sim, dim=-1)
+    nll = nll.mean()
+
+    return nll
+
+def neg_loss(logits, dim):
+    neg_ce = torch.diag(F.log_softmax(logits, dim=dim))
+    return -neg_ce.mean()
+    
+def clip_loss(similarity: torch.Tensor) -> torch.Tensor:
+    caption_loss = neg_loss(similarity, dim=0)
+    image_loss = neg_loss(similarity, dim=1)
+    return (caption_loss + image_loss) / 2.0
+
